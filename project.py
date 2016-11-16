59e899b30ed5af91a2b0f67371a9f175e135e1d3
AutoRun <osmstas@gmail.com>
2016-11-16 08:31:28 +0200

    finished project

--- project.py
+++ project.py
@@ -1,40 +1,30 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 from flask import render_template
-# from flask.ext.pymongo import PyMongo
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 
-
-# client = MongoClient('localhost:27017')
 client = MongoClient("localhost", 27017)
 db = client.zipcode
 cities = db.code
-
 
 app = Flask(__name__)
 mongo = PyMongo(app)
 
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World ggg123!'
-
 @app.route('/')
-def bestCities():
-    bestCities = []
-    for city in cities.find().sort("pop"):
-        bestCities.append(city)
-    bestCities = bestCities[-20:]
+def sort_cities():
 
-    for city in bestCities:
+    most_p_cities = [city for city in cities.find().sort("pop")][-20:]
+    for city in most_p_cities:
         for key in city.keys():
             if key == 'pop':
                 city['population'] = city[key]
                 del city['pop']
 
-    print '>>>>>>>>>>>>>>', bestCities
-    # cities = mongo.db.test.find().sort({"pop": -1})
-    return render_template('index.html', cities=bestCities)
+    return render_template('index.html', cities=most_p_cities)
+
 
 if __name__ == '__main__':
     app.run()
